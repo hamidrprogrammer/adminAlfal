af5e9a69c407de911947ff09d3e37a284112ec5a
Hamid Reza Alizadeh <hamidrdeveloper@gmail.com>
2024-11-03 09:18:43 +0300

    first commit

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -4,7 +4,7 @@ const msalConfig = {
     auth: {
         clientId: '356fe2ef-4e6c-4719-bfa5-1b2996c41fcd', // Application (client) ID
         authority: 'https://login.microsoftonline.com/d9f018ac-3fe4-43ee-b3ff-fdafdec35d3f', // Replace {tenantId} with your actual tenant ID
-        redirectUri: 'http://localhost:3000/api/auth/callback/azure-ad', // Redirect URI
+        redirectUri: 'https://admin.bu-fos-mastermind.solutions-apps.com/api/auth/callback/azure-ad', // Redirect URI
     },
     cache: {
         cacheLocation: 'localStorage', // This configures where your cache will be stored
